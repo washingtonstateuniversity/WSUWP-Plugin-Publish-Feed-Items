d440bee4aab39587a439c32b902a034313912d76
jeremyfelt <jeremy.felt@gmail.com>
2013-11-27 12:07:42 -0800

    Configure WordPress to use a new content directory

--- web/wp-config.php
+++ web/wp-config.php
@@ -33,6 +33,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define( 'WP_CONTENT_DIR', dirname( dirname( __FILE__ ) ) . '/content' );
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
