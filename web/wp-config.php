50b336c86c01479b7c02d79680fedff605f8f949
jeremyfelt <jeremy.felt@gmail.com>
2013-11-27 12:24:34 -0800

    Correct database for dev

--- web/wp-config.php
+++ web/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wsuwp_wnpa');
+define('DB_NAME', 'wsu_wnpa');
 
 /** MySQL database username */
 define('DB_USER', 'wp');
