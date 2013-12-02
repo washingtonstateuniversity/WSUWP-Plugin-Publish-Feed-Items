3cdddd2e23ee9f5e00129de33e1ac3ae8f014ea3
jeremyfelt <jeremy.felt@gmail.com>
2013-12-02 11:53:28 -0800

    Add initial plugin files for WNPA Syndication

---
+++ web/content/plugins/wnpa-syndication/wnpa-syndication.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WNPA Syndication
+ * Plugin URI: http://web.wsu.edu/
+ * Description: TBD
+ * Author: washingtonstateuniversity
+ * Version 0.1
+ * Author URI: http://web.wsu.edu
+ */
