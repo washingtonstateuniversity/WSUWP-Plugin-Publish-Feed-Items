f3b9cb5f184570d0efe9690830bd276eb53d14d9
jeremyfelt <jeremy.felt@gmail.com>
2013-12-02 11:14:13 -0800

    Correct WP_CONTENT_DIR setting to one directory up

--- web/wp-config.php
+++ web/wp-config.php
@@ -33,7 +33,7 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define( 'WP_CONTENT_DIR', dirname( dirname( __FILE__ ) ) . '/content' );
+define( 'WP_CONTENT_DIR', dirname( __FILE__ )  . '/content' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
