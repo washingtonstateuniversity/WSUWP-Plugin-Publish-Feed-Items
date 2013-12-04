e07f5e5f53b83787285586568c5e2676a5f406b9
jeremyfelt <jeremy.felt@gmail.com>
2013-12-03 16:23:35 -0800

    Initial handling of turning a Generate click into an Ajax request

--- web/content/plugins/wnpa-syndication/js/wnpa-admin.js
+++ web/content/plugins/wnpa-syndication/js/wnpa-admin.js
@@ -1,3 +1,27 @@
 (function( window, $ ) {
+	/**
+	 * Make an Ajax request to generate an access key for use in
+	 * the initiating user's profile.
+	 *
+	 * @param e The event associated with the 'Generate' click.
+	 */
+	function generate_access_key( e ) {
+		e.preventDefault();
 
+		var data = {
+			action: 'wnpa_generate_access_key'
+		};
+
+		$.post( ajaxurl, data, handle_response );
+	}
+
+	/**
+	 * Handle the response received from WordPress
+	 *
+	 * @param response
+	 */
+	function handle_response( response ) {
+	}
+
+	$( '#wnpa-generate-key' ).on( 'click', generate_access_key );
 })( window, jQuery );
