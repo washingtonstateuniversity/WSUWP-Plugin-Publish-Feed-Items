e34dcbae6b7c226b1f79c53a9e0ebc3e38e0f3dc
jeremyfelt <jeremy.felt@gmail.com>
2014-01-06 15:44:59 -0800

    Introduce the class to be used for external sources

---
+++ web/wp-content/plugins/wnpa-syndication/includes/class-wnpa-external-source.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Class WNPA_External_Source
+ *
+ * Track external feeds to parse on a regular basis for news items to be added as
+ * feed items to be consumed by others.
+ */
+class WNPA_External_Source {
+
+}
+global $wnpa_external_source;
+$wnpa_external_source = new WNPA_External_Source();
