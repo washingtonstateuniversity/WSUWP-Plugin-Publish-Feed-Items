23eb3c9c3eb64be230070155074088e53bed15a5
jeremyfelt <jeremy.felt@gmail.com>
2014-02-10 11:31:55 -0800

    Add a description to the plugin header

--- wp-content/plugins/wnpa-syndication/wnpa-syndication.php
+++ wp-content/plugins/wnpa-syndication/wnpa-syndication.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WNPA Syndication
  * Plugin URI: http://web.wsu.edu/
- * Description: TBD
+ * Description: Manage and republish content from multiple external sources into a single consumable feed.
  * Author: washingtonstateuniversity
  * Version 0.1
  * Author URI: http://web.wsu.edu
