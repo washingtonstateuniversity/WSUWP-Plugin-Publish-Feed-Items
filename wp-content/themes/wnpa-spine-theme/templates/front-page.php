1bdb1daae8ed4c66956f840e140216932ca9bc62
jeremyfelt <jeremy.felt@gmail.com>
2014-03-26 16:07:50 -0700

    Add a basic display of recent articles on the home page

---
+++ wp-content/themes/wnpa-spine-theme/templates/front-page.php
@@ -0,0 +1,51 @@
+<?php /* Template Name: WNPA Front Page */ ?>
+
+<?php get_header(); ?>
+
+	<main class="spine-single-template">
+
+		<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part('parts/headers'); ?>
+
+			<section class="row single">
+
+				<div class="column one">
+
+					<?php get_template_part('articles/article'); ?>
+
+				</div><!--/column-->
+
+			</section>
+
+		<?php endwhile; endif; ?>
+
+		<section class="row single recent-articles">
+			<div class="column one">
+				<h2 class="article-title">Recent Articles</h2>
+				<?php
+				$recent_articles = new WP_Query( array(
+					'post_type' => 'wnpa_feed_item',
+					'posts_per_page' => '4',
+				));
+				if ( $recent_articles->have_posts() ) : while( $recent_articles->have_posts() ) : $recent_articles->the_post();
+					$link_url = get_post_meta( get_the_ID(), '_feed_item_link_url', true );
+					$link_author = ucwords( strtolower( get_post_meta( get_the_ID(), '_feed_item_author', true ) ) );
+					$source_id = get_post_meta( get_the_ID(), '_feed_item_source', true );
+					$source = get_post( absint( $source_id ) );
+					?>
+					<div class="recent-article">
+						<h3 class="recent-article-title"><a href="<?php echo esc_url( $link_url ); ?>"><?php the_title(); ?></a></h3>
+						<span class="recent-article-date"><?php echo get_the_date(); ?></span>
+						<span class="recent-article-author"><?php echo esc_html( $link_author ); ?></span>
+						<span class="recent-article-source"><?php echo $source->post_title; ?></span>
+						<p class="recent-article-excerpt"><?php the_excerpt(); ?></p>
+					</div>
+				<?php endwhile; endif;
+				wp_reset_postdata(); ?>
+				</ul>
+			</div>
+		</section>
+	</main>
+
+<?php get_footer(); ?>
