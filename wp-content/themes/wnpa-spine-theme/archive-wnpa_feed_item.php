d662c19c2cc8ba45c60b10f82b2091f9a8685afd
jeremyfelt <jeremy.felt@gmail.com>
2014-05-12 10:54:07 -0700

    Fill link_url for manual items as well

--- wp-content/themes/wnpa-spine-theme/archive-wnpa_feed_item.php
+++ wp-content/themes/wnpa-spine-theme/archive-wnpa_feed_item.php
@@ -9,12 +9,11 @@
 					<?php while ( have_posts() ) : the_post();
 						$link_url = get_post_meta( get_the_ID(), '_feed_item_link_url', true );
 						$link_author = ucwords( strtolower( get_post_meta( get_the_ID(), '_feed_item_author', true ) ) );
-						if ( ! $link_author ) {
-							$link_author = 'By ' . get_the_author();
-						}
 						$source_id = get_post_meta( get_the_ID(), '_feed_item_source', true );
 						if ( empty( $source_id ) ) {
 							$source_title = 'Manual Entry';
+							$link_url = get_the_permalink();
+							$link_author = 'By ' . get_the_author();
 						} else {
 							$source = get_post( absint( $source_id ) );
 							$source_title = $source->post_title;
